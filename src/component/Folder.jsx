7ac0b12f84c9a0a64bbaea790ce32bd6aad83777
Sagar Bhatia <sagarbhatia722@gmail.com>
2023-10-26 11:26:12 +0530

    vs look

---
+++ src/component/Folder.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Folder = ({ explorer }) => {
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+
+  const addNewFolder = () => {};
+
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div className="folder" onClick={() => setExpand(!expand)}>
+          <span>📁{explorer.name}</span>
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
+        </div>
+        <div style={{ display: expand ? "block" : "none", marginLeft: 30 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                autoFocus
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                className="inputContainer__input"
+              />
+            </div>
+          )}
+          {explorer.items.map((exp) => (
+            <Folder explorer={exp} key={exp.id} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">📄{explorer.name}</span>;
+  }
+};
+
+export default Folder;
